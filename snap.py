c904c219e7d08aa99ebcbe85700da446a2cc4a9a
Technoboy10 <cchudson98@gmail.com>
2013-03-10 13:02:06 -0600

    Update snap.py

--- snap.py
+++ snap.py
@@ -1,16 +1,6 @@
 import SimpleHTTPServer
 class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def send_head(self):
-        """Common code for GET and HEAD commands.
-
-        This sends the response code and MIME headers.
-
-        Return value is either a file object (which has to be copied
-        to the outputfile by the caller unless the command was HEAD,
-        and must be closed by the caller under all circumstances), or
-        None, in which case the caller has nothing further to do.
-
-        """
 	path = self.path
 	print path
 	ospath = os.path.abspath('')
@@ -93,41 +83,6 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	        self.send_header("Access-Control-Allow-Origin", "*")
 	        self.end_headers()
 	        return f
-	else:
-	        f = None
-	        if os.path.isdir(path):
-	            if not self.path.endswith('/'):
-	                # redirect browser - doing basically what apache does
-	                self.send_response(301)
-	                self.send_header("Location", self.path + "/")
-	                self.end_headers()
-	                return None
-	            for index in "index.html", "index.htm":
-	                index = os.path.join(path, index)
-	                if os.path.exists(index):
-	                    path = index
-	                    break
-	            else:
-	                return self.list_directory(path)
-	        ctype = self.guess_type(path)
-	        try:
-	            # Always read in binary mode. Opening files in text mode may cause
-	            # newline translations, making the actual size of the content
-	            # transmitted *less* than the content-length!
-	            f = open(path, 'rb')
-	        except IOError:
-	            self.send_error(404, "File not found")
-	            return None
-	        self.send_response(200)
-	        self.send_header("Content-type", ctype)
-	        fs = os.fstat(f.fileno())
-	        self.send_header("Content-Length", str(fs[6]))
-	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-	        self.send_header("Access-Control-Allow-Origin", "*")
-	        self.end_headers()
-	        return f
-		print 'OK'
-
 
 if __name__ == "__main__":
     import re
@@ -164,5 +119,6 @@ if __name__ == "__main__":
     httpd = SocketServer.TCPServer(("", PORT), Handler)
 
     print "serving at port", PORT
+    print "Go ahead and launch Snap!."
     httpd.serve_forever()
 
