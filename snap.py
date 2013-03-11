395447e5c58860f565f6684f9289598b8a5b6add
Technoboy10 <technoboy10@fake.email>
2013-03-11 11:40:23 -0600

    Modified stuff

--- snap.py
+++ snap.py
@@ -83,6 +83,20 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	        self.send_header("Access-Control-Allow-Origin", "*")
 	        self.end_headers()
 	        return f
+	elif path == '/nxtbattery': #battery level
+		f = open(ospath + '/return', 'w+')
+		f.write(str(b.get_battery_level()))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+	        return f
 
 if __name__ == "__main__":
     import re
